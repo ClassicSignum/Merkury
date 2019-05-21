b9eaf825336159ff57d0d5089895d0370f7f3f70
ClassicSignum <47184720+ClassicSignum@users.noreply.github.com>
2019-05-21 16:33:05 +0600

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    // javascript is ready to run now
+
+    $('.first-box').hover(function(){
+        $(this).css({
+            cursor:"grab"
+        });
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
